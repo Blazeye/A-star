e0e85829e2a53ce233f9427cc68338cc0b5a3e25
Blazeye <53467795+Blazeye@users.noreply.github.com>
2019-10-14 05:35:21 +0200

    Add files via upload

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+
+    Grid grid;
+
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    private void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        // openSet have nodes with calculated F cost (they are potential choices)
+        // closedSet have nodes that have already been evaluated 
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost ||
+                    openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+            {
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach(Node neighbor in grid.GetNeighbors(currentNode))
+            {
+                if(!neighbor.walkable || closedSet.Contains(neighbor))
+                {
+                    continue;
+                }
+
+                // calculate updated gCost for when its changed when moving
+                int newMovementCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
+                // if new path to neighbor is shorter or the neighbor is not in the open set
+                if (newMovementCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor))
+                {
+                    neighbor.gCost = newMovementCostToNeighbor;
+                    neighbor.hCost = GetDistance(neighbor, targetNode);
+                    neighbor.parent = currentNode;
+
+                    if (!openSet.Contains(neighbor))
+                        openSet.Add(neighbor);
+                }
+            }
+        }
+    }
+
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+         while(currentNode != startNode)
+         {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+         }
+
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int distX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        // returns GCost = 14x + 10(x - y) 
+        // diagonals = 14 (diagonal length of 10x10 square), horz/vert = 10 for GCost/HCost 
+        // if x > y, use y to count nr of diagonal steps from A to B
+        if(distX > distY)
+        {
+            return 14 * distY + 10 * (distX - distY);
+        }
+        // if y > x, use x to count nr of diagonals steps 
+        return 14 * distX + 10 * (distY - distX);
+    }
+}
